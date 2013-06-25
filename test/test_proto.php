8a84ee1e819d9be71dc09a3a29f0e59f7268639c
Amaury Bouchard <amaury@amaury.net>
2013-06-26 00:54:02 +0200

    Simple benchmark.

--- test/test_proto.php
+++ test/test_proto.php
@@ -8,61 +8,82 @@
  * @copyright	© 2013, Amaury Bouchard
  */
 
+require_once('finebase/Ansi.php');
 require_once('finebase/FineTimer.php');
-
-const HOST = 'localhost';
-const PORT = 11138;
-
-$timer = new FineTimer();
-
-print("PUT test\n");
-$sock = fsockopen(HOST, PORT);
-$timer->start();
-commandPut($sock, 'aaa', 'xyz');
-commandPut($sock, 'ccc', 'ddd');
-commandPut($sock, 'eee', 'fff');
-$timer->stop();
-print("...OK (" . $timer->getTime() . ")\n");
-fclose($sock);
-
-print("GET test\n");
-$sock = fsockopen(HOST, PORT);
-$timer->start();
-commandGet($sock, 'aaa');
-commandGet($sock, 'ccc');
-commandGet($sock, 'eee');
-$timer->stop();
-print("...OK (" . $timer->getTime() . ")\n");
-fclose($sock);
-
-// compare with REDIS
 require_once('finebase/FineDatasource.php');
 require_once('finebase/FineNDB.php');
+require_once('finebase/FineCache.php');
 
-const REDIS_DSN = 'redis://serv1.finemedia.fr:6379/0';
+const FINEDB_HOST = 'localhost';
+const FINEDB_PORT = 11138;
+const REDIS_DSN = 'redis://localhost:6379/0';
+const MEMCACHE_DSN = 'memcache://localhost:11211';
 
+$nbrLoops = 15;
+$dataList = array(
+	'aaa'			=> 'bbb',
+	'trululu pouet pouet'	=> 'tagada tsouin tsouin',
+	'foobar'		=> 'fucked up beyond any recovery',
+	'Pere Noel'		=> 'Santa Claus'
+);
+$timer = new FineTimer();
 $ndb = FineNDB::factory(REDIS_DSN);
+$cache = FineCache::factory(MEMCACHE_DSN);
 
-print("REDIS PUT\n");
+// FineDB
+print(Ansi::bold("FineDB\n"));
+$sock = fsockopen(FINEDB_HOST, FINEDB_PORT);
 $timer->start();
-$ndb->set('aaa', 'xyz');
-$ndb->set('ccc', 'ddd');
-$ndb->set('eee', 'fff');
+for ($i = 0; $i < $nbrLoops; $i++) {
+	foreach ($dataList as $key => $data)
+		commandPut($sock, "$key-$i", $data);
+}
 $timer->stop();
-print("...OK (" . $timer->getTime() . ")\n");
-
-print("REDIS GET\n");
-$result = $ndb->get('aaa');
-print("'aaa' => '$result'\n");
-$result = $ndb->get('ccc');
-print("'ccc' => '$result'\n");
-$result = $ndb->get('eee');
-print("'eee' => '$result'\n");
+print("PUT : " . $timer->getTime() . "\n");
+fclose($sock);
+$sock = fsockopen(FINEDB_HOST, FINEDB_PORT);
+$timer->start();
+for ($i = 0; $i < $nbrLoops; $i++) {
+	foreach ($dataList as $key => $data)
+		commandGet($sock, "$key-$i");
+}
 $timer->stop();
-print("...OK (" . $timer->getTime() . ")\n");
+print("GET : " . $timer->getTime() . "\n");
+fclose($sock);
 
-require_once('finebase/FineNDB.php');
+// Redis
+print(Ansi::bold("Redis\n"));
+$timer->start();
+for ($i = 0; $i < $nbrLoops; $i++) {
+	foreach ($dataList as $key => $data)
+		$ndb->set("$key-$i", $data);
+}
+$timer->stop();
+print("PUT : " . $timer->getTime() . "\n");
+$timer->start();
+for ($i = 0; $i < $nbrLoops; $i++) {
+	foreach ($dataList as $key => $data)
+		$ndb->get("$key-$i");
+}
+$timer->stop();
+print("GET : " . $timer->getTime() . "\n");
 
+// Memcache
+print(Ansi::bold("Memcache\n"));
+$timer->start();
+for ($i = 0; $i < $nbrLoops; $i++) {
+	foreach ($dataList as $key => $data)
+		$cache->set("$key-$i", $data);
+}
+$timer->stop();
+print("PUT : " . $timer->getTime() . "\n");
+$timer->start();
+for ($i = 0; $i < $nbrLoops; $i++) {
+	foreach ($dataList as $key => $data)
+		$result = $cache->get("$key-$i");
+}
+$timer->stop();
+print("GET : " . $timer->getTime() . "\n");
 
 /**
  * Send data to FineDB server.
@@ -75,7 +96,7 @@ function commandPut($sock, $key, $data) {
 		  pack('N', mb_strlen($data, 'ascii')) . $data;
 	fwrite($sock, $buffer);
 	$response = fread($sock, 4096);
-	showResponse($response);
+	//showResponse($response);
 }
 /**
  * Fetch data from FineDB server.
@@ -86,8 +107,8 @@ function commandGet($sock, $key) {
 	$buffer = chr(2) . pack('n', mb_strlen($key, 'ascii')) . $key;
 	fwrite($sock, $buffer);
 	$response = fread($sock, 4096);
-	showResponse($response);
-	print("'$key' => '" . mb_substr($response, 5, 4096, 'ascii') . "'\n");
+	//showResponse($response);
+	//print("'$key' => '" . mb_substr($response, 5, 4096, 'ascii') . "'\n");
 }
 /**
  * Show the status of a response from FineDB server.
