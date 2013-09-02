c11436565d00ecdf7fdef5652589de5cbf0978fa
Amaury <amaury@amaury.net>
2013-09-02 18:03:58 +0200

    Add line edition on CLI client.

--- src/cli/finedb-cli.c
+++ src/cli/finedb-cli.c
@@ -10,8 +10,12 @@
 #include <stdarg.h>
 #include <string.h>
 #include <strings.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <errno.h>
 #include "ydefs.h"
 #include "libfinedb.h"
+#include "linenoise.h"
 
 /**
  * Options structure.
@@ -30,6 +34,9 @@ typedef struct cli_s {
 /** @define DEFAULT_HOST Default hostname of the server. */
 #define DEFAULT_HOST	"localhost"
 
+/** @define HISTORY_FILE Name of the command-line history file. */
+#define HISTORY_FILE	".finedb-cli.history"
+
 /** @define LTRIM Move forward a pointer to skip spaces. */
 #define LTRIM(pt)	while(*pt && IS_SPACE(*pt)) ++pt;
 
@@ -37,6 +44,7 @@ typedef struct cli_s {
 void printf_decorated(const char *type, char *s, ...);
 void printf_color(const char *color, char *s, ...);
 void print_response(char c);
+void cli_completion(const char *buf, linenoiseCompletions *lc);
 void command_help(void);
 void command_use(cli_t *cli, char *pt);
 void command_get(cli_t *cli, char *pt);
@@ -57,57 +65,104 @@ void command_async(cli_t *cli);
 void command_autocheck(cli_t *cli, char *pt);
 int check_connection(cli_t *cli);
 
+/* Array of commands. */
+char *commands[] = {
+	"help", "use", "get", "del", "put", "add", "update", "inc", "dec",
+	"start", "commit", "rollback", "ping", "sync", "async", "autocheck",
+	NULL
+};
+
 /* Main function. */
 int main(int argc, char *argv[]) {
 	cli_t cli;
-	char buff[8196], *hostname = DEFAULT_HOST;
-	size_t bufsz;
+	char *hostname = DEFAULT_HOST;
+	char history_file[4096];
+	ybool_t interactive_mode = YTRUE;
 
 	bzero(&cli, sizeof(cli_t));
 	cli.autocheck = YTRUE;
-	if (argc == 2)
+	if (argc == 2 && argv[1][0] != '-')
 		hostname = argv[1];
+	if (argc == 3 && !strcmp(argv[2], "-"))
+		interactive_mode = YFALSE;
+	// init database connection
 	if ((cli.finedb = finedb_create(hostname, 11138)) == NULL) {
 		printf_color("red", "Memory error.");
+		printf("\n");
 		exit(1);
 	}
 	if (finedb_connect(cli.finedb) != FINEDB_OK) {
-		printf_color("red", "Unable to connect to server '%s' on port '%d'.\n", argv[1], 11138);
+		printf_color("red", "Unable to connect to server '%s' on port '%d'.", argv[1], 11138);
+		printf("\n");
 		exit(2);
+	}
+	// interactive mode init
+	if (interactive_mode) {
+		char *home = NULL;
+
+		if ((home = getenv("HOME")) != NULL) {
+			FILE *hist;
+
+			snprintf(history_file, sizeof(history_file), "%s/%s", home, HISTORY_FILE);
+			if ((hist = fopen(history_file, "w+")) != NULL) {
+				fclose(hist);
+				linenoiseHistoryLoad(HISTORY_FILE);
+			}
+			linenoiseSetCompletionCallback(cli_completion);
+		}
 	}
 	// main loop
 	for (; ; ) {
-		char *pt, *cmd;
+		char buff[4096], *line = NULL, *pt, *cmd;
 
-		bzero(buff, sizeof(buff));
-		printf_decorated("bold", (cli.dbname ? cli.dbname : "default"));
-		printf(" > ");
-		fflush(stdout);
-		bufsz = read(0, buff, 8192);
-		buff[bufsz - 1] = '\0';
+		if (!interactive_mode) {
+			ssize_t bufsz, linesz = 0;
 
-		pt = buff;
+			while ((bufsz = read(0, buff, sizeof(buff))) > 0) {
+				pt = (char*)malloc(linesz + bufsz + 1);
+				memcpy(pt, line, linesz);
+				memcpy((void*)((size_t)pt + linesz), buff, bufsz);
+				linesz += bufsz;
+				pt[linesz] = '\0';
+				if (line)
+					free(line);
+				line = pt;
+			}
+		} else {
+			snprintf(buff, sizeof(buff), "%s > ", (cli.dbname ? cli.dbname : "default"));
+			if ((line = linenoise(buff)) == NULL)
+				break;
+		}
+		pt = line;
 		LTRIM(pt);
 		cmd = pt;
+		// add command line to history
+		linenoiseHistoryAdd(cmd);
+		linenoiseHistorySave(history_file);
+		// extract the command
 		while (*pt && !IS_SPACE(*pt))
 			++pt;
 		*pt++ = '\0';
 		LTRIM(pt);
 		/* command management */
 		if (cmd[0] == '\0')
-			continue;
+			goto reloop;
+			//continue;
 		// local commands, no need for a running connection
 		if (!strcasecmp(cmd, "exit") || !strcasecmp(cmd, "quit"))
 			exit(0);
 		if (!strcasecmp(cmd, "help") || cmd[0] == '?') {
 			command_help();
-			continue;
+			goto reloop;
+			//continue;
 		} else if (!strcasecmp(cmd, "sync")) {
 			command_sync(&cli);
-			continue;
+			goto reloop;
+			//continue;
 		} else if (!strcasecmp(cmd, "async")) {
 			command_async(&cli);
-			continue;
+			goto reloop;
+			//continue;
 		}
 		// commands that need a running connection
 		if (!strcasecmp(cmd, "use"))
@@ -140,8 +195,12 @@ int main(int argc, char *argv[]) {
 			command_ping(&cli);
 		else if (!strcasecmp(cmd, "autocheck"))
 			command_autocheck(&cli, pt);
-		else
-			printf_color("red", "Bad command.\n");
+		else {
+			printf_color("red", "Bad command.");
+			printf("\n");
+		}
+reloop:
+		free(line);
 	}
 	return (0);
 }
@@ -164,6 +223,7 @@ void command_help() {
 	                          "    async\n"
 	                          "    autocheck [on|off]\n"
 	                          "    quit\n");
+	printf("\n");
 }
 
 /* Defines the used database. */
@@ -176,20 +236,24 @@ void command_use(cli_t *cli, char *pt) {
 	// request
 	if (!strlen(pt) || !strcasecmp(pt, "default")) {
 		// use default database
-		if ((rc = finedb_setdb(cli->finedb, NULL)) != 0)
-			printf_color("red", "Unable to use the default database (%d).\n", rc);
-		else {
+		if ((rc = finedb_setdb(cli->finedb, NULL)) != 0) {
+			printf_color("red", "Unable to use the default database (%d).", rc);
+			printf("\n");
+		} else {
 			YFREE(cli->dbname);
-			printf_decorated("faint", "Use the default database\n");
+			printf_decorated("faint", "Use the default database");
+			printf("\n");
 		}
 	} else {
 		// set a new database
-		if ((rc = finedb_setdb(cli->finedb, pt)) != 0)
-			printf_color("red", "Unable to use the '%s' database (%d).\n", rc);
-		else {
+		if ((rc = finedb_setdb(cli->finedb, pt)) != 0) {
+			printf_color("red", "Unable to use the '%s' database (%d).", rc);
+			printf("\n");
+		} else {
 			YFREE(cli->dbname);
 			cli->dbname = strdup(pt);
-			printf_decorated("faint", "Use the '%s' database\n", pt);
+			printf_decorated("faint", "Use the '%s' database", pt);
+			printf("\n");
 		}
 	}
 }
@@ -197,12 +261,14 @@ void command_use(cli_t *cli, char *pt) {
 /* Set synchronous mode. */
 void command_sync(cli_t *cli) {
 	finedb_sync(cli->finedb);
-	printf_decorated("faint", "Set synchronous mode.\n");
+	printf_decorated("faint", "Set synchronous mode.");
+	printf("\n");
 }
 /* Set asynchronous mode. */
 void command_async(cli_t *cli) {
 	finedb_async(cli->finedb);
-	printf_decorated("faint", "Set asynchronous mode.\n");
+	printf_decorated("faint", "Set asynchronous mode.");
+	printf("\n");
 }
 
 /* Fetch a value from its key. */
@@ -213,18 +279,21 @@ void command_get(cli_t *cli, char *pt) {
 
 	LTRIM(pt);
 	if (*pt != '"') {
-		printf_decorated("faint", "Bad key format (no quote)\n");
+		printf_decorated("faint", "Bad key format (no quote)");
+		printf("\n");
 		return;
 	}
 	pt++;
 	if (!*pt) {
-		printf_decorated("faint", "Bad key\n");
+		printf_decorated("faint", "Bad key");
+		printf("\n");
 		return;
 	}
 	key = pt2 = pt;
 	pt2 = pt;
 	if ((pt2 = strchr(pt2, '"')) == NULL) {
-		printf_decorated("faint", "Bad key format (no trailing quote)\n");
+		printf_decorated("faint", "Bad key format (no trailing quote)");
+		printf("\n");
 		return;
 	}
 	*pt2 = '\0';
@@ -237,11 +306,14 @@ void command_get(cli_t *cli, char *pt) {
 	ybin_set(&bkey, key, strlen(key));
 	rc = finedb_get(cli->finedb, bkey, &bdata);
 	if (rc) {
-		printf_color("red", "Unable to get key '%s' (%d).\n", key, rc);
+		printf_color("red", "Unable to get key '%s' (%d).", key, rc);
+		printf("\n");
 		return;
 	}
-	if (bdata.data == NULL || bdata.len == 0)
-		printf_decorated("faint", "No data.\n");
+	if (bdata.data == NULL || bdata.len == 0) {
+		printf_decorated("faint", "No data.");
+		printf("\n");
+	}
 	printf("%s\n", (char*)bdata.data);
 }
 
@@ -253,18 +325,21 @@ void command_del(cli_t *cli, char *pt) {
 
 	LTRIM(pt);
 	if (*pt != '"') {
-		printf_decorated("faint", "Bad key format (no quote)\n");
+		printf_decorated("faint", "Bad key format (no quote)");
+		printf("\n");
 		return;
 	}
 	pt++;
 	if (!*pt) {
-		printf_decorated("faint", "Bad key\n");
+		printf_decorated("faint", "Bad key");
+		printf("\n");
 		return;
 	}
 	key = pt2 = pt;
 	pt2 = pt;
 	if ((pt2 = strchr(pt2, '"')) == NULL) {
-		printf_decorated("faint", "Bad key format (no trailing quote)\n");
+		printf_decorated("faint", "Bad key format (no trailing quote)");
+		printf("\n");
 		return;
 	}
 	*pt2 = '\0';
@@ -276,8 +351,10 @@ void command_del(cli_t *cli, char *pt) {
 	ybin_set(&bkey, key, strlen(key));
 	rc = finedb_del(cli->finedb, bkey);
 	if (rc)
-		printf_color("red", "Unable to delete key '%s'.\n", key);
-	printf_decorated("faint", "OK\n");
+		printf_color("red", "Unable to delete key '%s'.", key);
+		printf("\n");
+	printf_decorated("faint", "OK");
+	printf("\n");
 }
 
 /* Put, add or update a key/value in database. */
@@ -288,41 +365,48 @@ void command_send_data(cli_t *cli, char *pt, ybool_t create_only, ybool_t update
 
 	LTRIM(pt);
 	if (*pt != '"') {
-		printf_decorated("faint", "Bad key format (no quote)\n");
+		printf_decorated("faint", "Bad key format (no quote)");
+		printf("\n");
 		return;
 	}
 	pt++;
 	if (!*pt) {
-		printf_decorated("faint", "Bad key\n");
+		printf_decorated("faint", "Bad key");
+		printf("\n");
 		return;
 	}
 	key = pt2 = pt;
 	pt2 = pt;
 	if ((pt2 = strchr(pt2, '"')) == NULL) {
-		printf_decorated("faint", "Bad key format (no trailing quote)\n");
+		printf_decorated("faint", "Bad key format (no trailing quote)");
+		printf("\n");
 		return;
 	}
 	*pt2 = '\0';
 	pt2++;
 	if (!*pt2) {
-		printf_decorated("faint", "Missing data\n");
+		printf_decorated("faint", "Missing data");
+		printf("\n");
 		return;
 	}
 	*pt2 = '\0';
 	data = pt2 + 1;
 	LTRIM(data);
 	if (*data != '"') {
-		printf_decorated("faint", "Bad data format (no quote)\n");
+		printf_decorated("faint", "Bad data format (no quote)");
+		printf("\n");
 		return;
 	}
 	data++;
 	if (!*data) {
-		printf_decorated("faint", "Missing data\n");
+		printf_decorated("faint", "Missing data");
+		printf("\n");
 		return;
 	}
 	pt2 = data + strlen(data) - 1;
 	if (*pt2 != '"') {
-		printf_decorated("faint", "Bad data format (no trailing quote)\n");
+		printf_decorated("faint", "Bad data format (no trailing quote)");
+		printf("\n");
 		return;
 	}
 	*pt2 = '\0';
@@ -340,9 +424,11 @@ void command_send_data(cli_t *cli, char *pt, ybool_t create_only, ybool_t update
 	else
 		rc = finedb_put(cli->finedb, bkey, bdata);
 	if (rc)
-		printf_color("red", "Unable to %s key '%s'.\n",
+		printf_color("red", "Unable to %s key '%s'.",
 		             (create_only ? "add" : (update_only ? "update" : "put")), key);
-	printf_decorated("faint", "OK\n");
+	else
+		printf_decorated("faint", "OK");
+	printf("\n");
 }
 
 /* Increment a value. */
@@ -362,15 +448,18 @@ void command_start(cli_t *cli) {
 		return;
 	// check opened transaction
 	if (cli->in_transaction) {
-		printf_color("red", "A transaction is already open. It will be rollbacked.\n");
+		printf_color("red", "A transaction is already open. It will be rollbacked.");
+		printf("\n");
 	}
 	// request
 	rc = finedb_start(cli->finedb);
 	if (rc) {
-		printf_color("red", "Server error.\n");
+		printf_color("red", "Server error.");
+		printf("\n");
 		return;
 	}
-	printf_decorated("faint", "Transaction started.\n");
+	printf_decorated("faint", "Transaction started.");
+	printf("\n");
 	cli->in_transaction = YTRUE;
 }
 
@@ -383,16 +472,19 @@ void command_commit(cli_t *cli) {
 		return;
 	// check opened transaction
 	if (!cli->in_transaction) {
-		printf_color("red", "No opened transaction.\n");
+		printf_color("red", "No opened transaction.");
+		printf("\n");
 		return;
 	}
 	// request
 	rc = finedb_commit(cli->finedb);
 	if (rc) {
-		printf_color("red", "Server error.\n");
+		printf_color("red", "Server error.");
+		printf("\n");
 		return;
 	}
-	printf_decorated("faint", "Transaction committed.\n");
+	printf_decorated("faint", "Transaction committed.");
+	printf("\n");
 	cli->in_transaction = YFALSE;
 }
 
@@ -405,16 +497,19 @@ void command_rollback(cli_t *cli) {
 		return;
 	// check opened transaction
 	if (!cli->in_transaction) {
-		printf_color("red", "No opened transaction.\n");
+		printf_color("red", "No opened transaction.");
+		printf("\n");
 		return;
 	}
 	// request
 	rc = finedb_rollback(cli->finedb);
 	if (rc) {
-		printf_color("red", "Server error.\n");
+		printf_color("red", "Server error.");
+		printf("\n");
 		return;
 	}
-	printf_decorated("faint", "Transaction aborted.\n");
+	printf_decorated("faint", "Transaction aborted.");
+	printf("\n");
 	cli->in_transaction = YFALSE;
 }
 
@@ -422,16 +517,15 @@ void command_rollback(cli_t *cli) {
 void command_ping(cli_t *cli) {
 	int rc;
 
-	// check connection if needed
-	if (!check_connection(cli))
-		return;
 	// request
 	rc = finedb_ping(cli->finedb);
 	if (rc) {
-		printf_color("red", "Server error.\n");
+		printf_color("red", "Server error.");
+		printf("\n");
 		return;
 	}
-	printf_decorated("faint", "OK\n");
+	printf_decorated("faint", "OK");
+	printf("\n");
 }
 
 /* Set the autocheck option. */
@@ -443,8 +537,9 @@ void command_autocheck(cli_t *cli, char *pt) {
 		else
 			cli->autocheck = YFALSE;
 	}
-	printf_decorated("faint", "autocheck option is %s\n",
+	printf_decorated("faint", "autocheck option is %s",
 	                 cli->autocheck ? "activated" : "deactivated");
+	printf("\n");
 }
 
 /**
@@ -461,14 +556,16 @@ int check_connection(cli_t *cli) {
 		return (1);
 	// no connection, try to reconnect
 	if (finedb_connect(cli->finedb) != FINEDB_OK) {
-		printf_color("red", "Lost connection to server.\n");
+		printf_color("red", "Lost connection to server.");
+		printf("\n");
 		return (0);
 	}
 	if (cli->in_transaction) {
-		printf_decorated("faint", "Reconnected to server. Transaction is lost.\n");
 		cli->in_transaction = YFALSE;
+		printf_decorated("faint", "Reconnected to server. Transaction is lost.");
 	} else
-		printf_decorated("faint", "Reconnected to server.\n");
+		printf_decorated("faint", "Reconnected to server.");
+	printf("\n");
 	return (1);
 }
 
@@ -542,6 +639,27 @@ void command_list(cli_t *cli, char *pt) {
 	}
 }
 #endif
+
+/* ******************** UTILITIES FUNCTIONS ********************* */
+
+/**
+ * Command line completion.
+ * @param	buf	Text to complete.
+ * @param	lc	Readline object.
+ */
+void cli_completion(const char *buf, linenoiseCompletions *lc) {
+	unsigned int i;
+	size_t bufsz = strlen(buf);
+
+	for (i = 0; commands[i]; ++i) {
+		char *cmd = commands[i];
+
+		if (strlen(cmd) < bufsz)
+			continue;
+		if (!strncasecmp(buf, cmd, bufsz))
+			linenoiseAddCompletion(lc, cmd);
+	}
+}
 
 /**
  * Write a decorated string.
